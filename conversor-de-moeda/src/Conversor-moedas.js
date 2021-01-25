2bd12d9da7db5c14ce87c0c26650d2e37f828b78
Kaecio <56613892+Kaecio@users.noreply.github.com>
2021-01-25 07:11:02 -0300

    Instalando dependencias e implementando o corpo da aplicação

--- conversor-de-moeda/src/Conversor-moedas.js
+++ conversor-de-moeda/src/Conversor-moedas.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './conversor-moedas.css';
-import {Jumbotron, Button, Form, Col, Spinner} from 'react-bootstrap';
+import {Jumbotron, Button, Form, Col, Spinner, Alert, Modal} from 'react-bootstrap';
 
 
 function ConversorMoeda() {
   return (
     <div>
     <h1>Conversor de Moedas..</h1>
+    <Alert variant="danger" show={false}>
+      Erro obtendo moeda
+    </Alert>
     <Jumbotron>
       <Form>
         <Form.Row>
@@ -17,18 +20,32 @@ function ConversorMoeda() {
             <Form.Control as="select"></Form.Control>
           </Col>
           <Col sm="1" className="text-center" style={{paddingTop:"5px"}}>
-            <span>> ></span>
+            <span>`{`>>`}`</span>
           </Col>
           <Col sm="3">
           <Form.Control as="select"></Form.Control>
           </Col>
           <Col sm="2">
-            <Button variant="sucess" type="submit">
+            <Button variant="success" type="submit">
+            <Spinner animation="border" size="sm" />
             Converter
             </Button>
           </Col>
         </Form.Row>
       </Form>
+      <Modal show={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>Conversão</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Resultado da conversão aqui
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success">
+            Nova Conversão
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Jumbotron>
     </div>
   );
